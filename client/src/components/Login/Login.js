ecf5edda4822d57434389c823d8494816d3e64cc
Veronika Naydenova <veronica.naydenova@gmail.com>
2022-08-13 08:42:41 +0300

    firebase-deploy

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -44,7 +44,7 @@ export const Login = () => {
                 navigate('/');
             })
             .catch(() => {
-                return;
+                navigate('*');
             })
 
     };
